076a4f0175b3547d82ed80b4e67200f9fabfb7c8
anurudhc <anurudhc11@gmail.com>
2023-06-14 15:56:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDos, setTodos] = useState([]);
+  const [toDo, setTodo] = useState('');
+
+  function deleteTodo(id) {
+    setTodos(toDos.filter((todo) => todo.id !== id));
+  }
+
+  function addTodo() {
+    if (toDo.trim() === '') {
+      return; // Don't add empty todo
+    }
+
+    const isDuplicate = toDos.some((todo) => todo.text === toDo);
+    if (isDuplicate) {
+      return; // Don't add duplicate todo
+    }
+
+    setTodos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+    setTodo(''); // Clear the input field
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(e) => setTodo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i onClick={addTodo} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {toDos.map((obj) => (
+          <div className="todo" key={obj.id}>
+            <div className="left">
+              <input
+                onChange={(e) =>
+                  setTodos(
+                    toDos.map((todo) => {
+                      if (todo.id === obj.id) {
+                        return { ...todo, status: e.target.checked };
+                      }
+                      return todo;
+                    })
+                  )
+                }
+                value={obj.status}
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <p>{obj.text}</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times" onClick={() => deleteTodo(obj.id)}></i>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
